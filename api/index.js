79a21e81196d33edf6b3a6780f652a4412e532c7
Sandesh <sdsoni2042@gmail.com>
2024-07-08 17:25:14 +0530

    connected to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect("mongodb+srv://sahand:sahand@mern-estatec.hjbi9hl.mongodb.net/?retryWrites=true&w=majority&appName=mern-estatec")
+.then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000,()=>{
-    console.log('server at port 3');
+    console.log('server at port 3000');
 }
 );
