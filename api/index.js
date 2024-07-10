70205ff11b5fe1ec6a9b2f29c292d4259daeded9
Sandesh <sdsoni2042@gmail.com>
2024-07-10 15:46:16 +0530

    testing signup api(basic)

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 mongoose.connect("mongodb+srv://sahand:sahand@mern-estatec.hjbi9hl.mongodb.net/?retryWrites=true&w=majority&appName=mern-estatec")
 .then(() => {
@@ -12,9 +13,12 @@ mongoose.connect("mongodb+srv://sahand:sahand@mern-estatec.hjbi9hl.mongodb.net/?
 
 const app = express();
 
+// app.use(express.json());
+
 app.listen(3000,()=>{
     console.log('server at port 3000');
 }
 );
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
