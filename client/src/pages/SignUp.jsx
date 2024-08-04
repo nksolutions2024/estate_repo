685f178fc5ee69bca29ba7ca189c74dfc67962ee
Sandesh <sdsoni2042@gmail.com>
2024-08-04 18:42:16 +0530

    now SignUp page is looks OK

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
@@ -12,7 +13,7 @@ export default function SignUp() {
       ...formData,
       [e.target.id]: e.target.value,
     });
-  };
+  };  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -48,7 +49,9 @@ export default function SignUp() {
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
 
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+      <form
+        onSubmit={handleSubmit} 
+        className='flex flex-col gap-4'>
         <input type="text" placeholder='username' 
         className='border p-3 rounded-lg' id='username' onChange={handleChange} />
         <input type="email" placeholder='email' 
@@ -56,12 +59,12 @@ export default function SignUp() {
         <input type="password" placeholder='password' 
         className='border p-3 rounded-lg' id='password' onChange={handleChange} />
         
-        <button 
-          disabled={loading} 
+        <button
+          disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase 
           hover:opacity-95 disabled:opacity-80'
           >
-            {loading ? 'Loading...' : 'Sign Up'}
+            {loading ? 'loading...' : 'Sign Up'}
         </button>
       </form>
 
@@ -71,8 +74,7 @@ export default function SignUp() {
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
-      {error && <p className='text-red-500 mt-5'>{error}</p>}
-
+      {error && <p className='test-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
